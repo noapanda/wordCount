34c4884c6cd7cf487cc1bb68b1e87587abc4fa41
Fireflower <55024112+noapanda@users.noreply.github.com>
2019-10-19 17:53:28 +0800

    Add files via upload

--- 单词统计.cpp
+++ 单词统计.cpp
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <string.h>
- 
-int main(void)  
-{  
-        char str[] = "2015-3-22 abcd efg hij klmn opq##rst,uvw,xyz,tao";/*待分隔的原字符串*/  
+int word_count(char *str)  
+{   
         char *s=str;
+        int count = 0;
         
-        while(*s)
+        while(*s)//将分割符全部转换为空格 
         {
-        	if (*s<'0' || (*s>'9' && *s<'A') || (*s>'Z' && *s<'a') || *s>'z')
+        	if (*s<'0' || (*s>'9' && *s<'a') || *s>'z')
         	{
         		*s=' ';
         	}
@@ -24,21 +21,29 @@ int main(void)
 				i++;
                  
                 substr = strtok(NULL,seg);/*在第一次调用时，strtok()必需给予参数str字符串，
-				往后的调用则将参数str设置成NULL。每次调用成功则返回被分割出片段的指针。*/
-				//char *p = charlist;
-				int j=0;
-				int cou = 0;
-				for(j = 0; j < i; j++)
-				{
-					char *p = charlist[j];
-					while(*p)
-					{
-						if(*p<)
-					}
-				}
-				
-				
+				往后的调用则将参数str设置成NULL。每次调用成功则返回被分割出片段的指针。*/	
         } 
-		
-        return 0;  
+        int j=0;
+				
+		for(j = 0; j < i; j++)
+		{
+			char *p = charlist[j];//子字符串的首地址 
+			int cou = 0;
+			int k=0;
+			while(*p)
+			{
+				if(*p>='a' && *p<='z')//判断是否为英文字母 
+				    cou++;	
+				p ++;
+				k ++;
+				if(k == 4 && cou < 4)//查找到第四个字符，仍不满足至少以4个英文字母开头 
+				    break;
+				if(cou == 4)//满足至少以4个英文字母开头 
+				{
+					count ++;
+					break;
+				}
+			}
+		}
+	return count;
 }
